2995cefa7839e491d37407d276cc7618e878e88f
Jim <oberadin25@gmail.com>
2020-08-08 21:23:13 +0300

    title

---
+++ src/components/Title.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../css/Title.css'
+
+function Title() {
+  return (
+    <div className="Title">
+    <div>
+      <h1>SFPOPOS</h1>
+    </div>
+      <div className="Title-Subtitle">
+          <h3>San Francisco Privately Owned Public Open Spaces</h3>
+      </div>
+    </div>
+  )
+}
+
+export default Title
