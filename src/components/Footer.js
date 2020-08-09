276a7bbe1a975821edde8922db343f8b56fa8229
Jim <oberadin25@gmail.com>
2020-08-09 11:06:23 +0300

    footer data and like count

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,10 +2,14 @@ import React from 'react';
 import '../css/Footer.css'
 
 function Footer() {
+    let currentDate = new Date().toLocaleString();
     return (
         <div className="Footer">
             <p>
                 Adin O copyright 2020
+            </p>
+            <p>
+                {currentDate}
             </p>
         </div>
     )
--- src/components/PoposSpace.js
+++ src/components/PoposSpace.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import '../css/PoposSpace.css'
+import { useState } from 'react'
 
 function POPOSSpace(props) {
     const { name, image, address } = props
+    const [likeCount, setCount] = useState(0);
   return (
     <div className="POPOSSpace">
       <img src={`${process.env.PUBLIC_URL}images/${image}`} width="300" height="300" alt="Hello" />
       <h1>{name}</h1>
       <div>{address}</div>
+      <button onClick={() => setCount(likeCount + 1)}>Like</button>
+      <p>{likeCount}likes</p>
     </div>
   )
 }
